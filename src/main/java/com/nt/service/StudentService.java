c61d9e1555790d6ae023e174d146ea6ab1a6235f
LAPTOP-1B9QC4GQ\Amma <knagaraj1100@gmail.com>
2022-07-08 10:19:21 +0530

    MP-6655 [nagarjuna] create save api

--- src/main/java/com/nt/service/StudentService.java
+++ src/main/java/com/nt/service/StudentService.java
@@ -1,5 +1,17 @@
 package com.nt.service;
 
+import org.springframework.stereotype.Service;
+
+import com.nt.model.Student;
+import com.nt.repository.StudentRepository;
+
+@Service
 public class StudentService {
+	private StudentRepository studentrepo;
+	
+	public Student saveStudent(Student st) {
+		Student stt = studentrepo.save(st);
+		return stt;
+	}
 
 }

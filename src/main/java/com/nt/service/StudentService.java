91c9eb2c9a79dab07e17562b9811155dfd5de380
LAPTOP-1B9QC4GQ\Amma <knagaraj1100@gmail.com>
2022-07-08 12:15:59 +0530

    MP-3344[balaji]created the Student getId

--- src/main/java/com/nt/service/StudentService.java
+++ src/main/java/com/nt/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.nt.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.nt.model.Student;
@@ -12,6 +14,14 @@ public class StudentService {
 	public Student saveStudent(Student st) {
 		Student stt = studentrepo.save(st);
 		return stt;
+	}
+	public Student getStudentById(int id) {
+		Optional<Student> sts = studentrepo.findById(id);
+		Student st= null;
+		if(sts.isPresent()) {
+			st=sts.get();
+		}
+		return st;
 	}
 
 }
